8fb5c9835773688c02e938763f567f96a062d5e7
Josh <joshuaepenner@gmail.com>
2025-09-29 12:12:36 -0500

    add lsp, other config stuff

--- init.lua
+++ init.lua
@@ -182,7 +182,6 @@ require('lazy').setup({
   'sindrets/diffview.nvim',
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-
   {
     'AntonVanAssche/date-time-inserter.nvim',
     version = '*',
@@ -322,7 +321,10 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>s.', function()
+        builtin.oldfiles()
+        vim.api.nvim_feedkeys('<Esc>', 'i', false)
+      end, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
@@ -521,7 +523,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
