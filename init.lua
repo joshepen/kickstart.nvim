a7b2a49455b609bb63c89ca04ade5b10720809e1
Josh <joshuaepenner@gmail.com>
2025-12-27 14:14:33 -0600

    change theme, add plugin

--- init.lua
+++ init.lua
@@ -182,7 +182,6 @@ require('lazy').setup({
     end,
   },
   'sindrets/diffview.nvim',
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
   {
     'AntonVanAssche/date-time-inserter.nvim',
@@ -193,7 +192,17 @@ require('lazy').setup({
       date_time_separator = ' ',
     },
   },
-
+  {
+    'joshepen/blb.nvim',
+    config = function()
+      local blb = require 'blb'
+      blb.setup()
+      blb.config.translation = 'ESV'
+    end,
+    keys = {
+      { '<leader>b', '<Esc>:BLB<CR>', desc = 'Open Blue Letter Bible', mode = { 'n', 'v' } },
+    },
+  },
   -- Markdown
   {
     'MeanderingProgrammer/render-markdown.nvim',
@@ -711,25 +720,12 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  {
+    'rebelot/kanagawa.nvim',
+    priority = 1000,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      require('kanagawa').setup { transparent = true }
+      vim.cmd 'colorscheme kanagawa-dragon'
     end,
   },
 
